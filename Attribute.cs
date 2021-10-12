6a4b10cb06a3c186bf7f18861e54e7540b8c0d0e
Z <1508207252@qq.com>
2021-10-12 23:10:10 +0800

    怪物属性设置与赋值

---
+++ Attribute.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attribute : MonoBehaviour
+{
+    public string name_;
+    protected float hpMax = 10;
+    protected float 攻击CD = 10;
+    protected float moveSpeed = 1;
+    protected float 硬直时间 = 0.3f;
+    protected float 攻击概率 = 50;
+    protected float 追击概率 = 80;
+    protected float 发呆时间 = 1;
+    protected float 前摇时间 = 1;
+    protected float 普通生效时间 = 0.5f;
+    protected float 后摇时间 = 1;
+    protected float 视觉范围 = 5;
+    protected float 听觉范围 = 6;
+    protected float 攻击距离 = 2;
+    protected float 失去目标距离 = 8;
+    protected bool 是否看向主角 = false;
+}
